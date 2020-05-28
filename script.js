61932ee0325d03cc89df58be6db5378fba1c24b1
laurentcodes <o.sherkes@gmail.com>
2020-05-28 02:31:09 +0100

    Fixed Input Views on smaller screens and fixed Delete todo

--- script.js
+++ script.js
@@ -6,24 +6,27 @@ const todos = document.getElementById('todos');
 
 let todosData = JSON.parse(localStorage.getItem('todoSideTodos'));
 
+// Check if there's data in the localstorage and display if there is
 function checkLocalStorage() {
 	if ('todoSideTodos' in localStorage) {
 		updateDOM(todosData);
 	} else {
+		// If no data, display dummy data/welcome page
 		dummyData = [
 			'Welcome to TodoSide. 📝',
 			'Click New Todo to add a new Todo. ➕',
 			'Hover on a todo to show the delete button. 🚮',
 			'Click on a button to mark it as completed. ✅',
 		];
+
+		// Update DOM with dummy data
 		updateDOM(dummyData);
 
 		console.log('No Local Storage Item Available');
 	}
 }
 
-checkLocalStorage();
-
+// Update DOM with passed in data
 function updateDOM(data) {
 	todos.innerHTML = '';
 	data.forEach((el) => {
@@ -38,10 +41,12 @@ function updateDOM(data) {
 	});
 }
 
+// Show Input field to add todo
 addTodo.addEventListener('click', () => {
 	todoInput.classList.add('show');
 });
 
+// Save todo to storage and display in DOM
 submitTodo.addEventListener('click', (e) => {
 	const parent = e.target.parentElement;
 	let todoItem = parent.children[0].value;
@@ -53,6 +58,7 @@ submitTodo.addEventListener('click', (e) => {
   </li>
   `;
 
+	// Check if todosData array exists
 	todosData ? todosData : (todosData = []);
 
 	todosData.push(todoItem);
@@ -65,23 +71,49 @@ submitTodo.addEventListener('click', (e) => {
 
 	parent.children[0].value = '';
 
+	// Call checkLocalStorage function after adding new Data
 	setTimeout(checkLocalStorage, 1000);
 });
 
+// Adding new Element to DOm
 function addtoDOM(el) {
 	todos.insertAdjacentHTML('beforeend', el);
 }
 
+// Toggle completed and delete todo
 todos.addEventListener('click', (e) => {
+	// If the list item is clicked
 	if (e.target && e.target.matches('li')) {
 		e.target.classList.toggle('completed');
 	}
 
+	// If the delete button is clicked
 	if (e.target && e.target.matches('i')) {
 		const el = e.target.parentElement;
 		el.style.opacity = '0';
+
+		// Check if element selected is in the array and delete if found
+		for (let i = 0; i < todosData.length; i++) {
+			if (todosData[i] === el.innerText) {
+				todosData.splice(i, 1);
+			}
+		}
+
+		// Update local storage
+		localStorage.setItem('todoSideTodos', JSON.stringify(todosData));
+
+		// Remove dummy data whenever a new data is added
 		setTimeout(function () {
 			el.remove();
 		}, 300);
 	}
 });
+
+// Close the input modal if anywhere else on the screen is clicked
+document.addEventListener('mouseup', (e) => {
+	if (!todoInput.contains(e.target)) {
+		todoInput.classList.remove('show');
+	}
+});
+
+checkLocalStorage();
