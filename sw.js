847a5a4deb9799bbf4cdf8b83f90d91457b15d72
laurentcodes <o.sherkes@gmail.com>
2020-05-29 00:54:01 +0100

    Changed to Dynamic Cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const staticCacheName = 'site-static-v1';
+const dynamicCacheName = 'site-static-v2';
 const assets = [
 	'/',
 	'/index.html',
@@ -7,32 +7,43 @@ const assets = [
 	'/script.js',
 	'https://fonts.googleapis.com/css2?family=Rubik&display=swap',
 ];
+
 // install event
 self.addEventListener('install', (evt) => {
 	evt.waitUntil(
-		caches.open(staticCacheName).then((cache) => {
+		caches.open(dynamicCacheName).then((cache) => {
 			console.log('caching shell assets');
 			cache.addAll(assets);
 		})
 	);
 });
+
 // activate event
 self.addEventListener('activate', (evt) => {
 	evt.waitUntil(
 		caches.keys().then((keys) => {
 			return Promise.all(
 				keys
-					.filter((key) => key !== staticCacheName)
+					.filter((key) => key !== dynamicCacheName)
 					.map((key) => caches.delete(key))
 			);
 		})
 	);
 });
+
 // fetch event
 self.addEventListener('fetch', (evt) => {
 	evt.respondWith(
 		caches.match(evt.request).then((cacheRes) => {
-			return cacheRes || fetch(evt.request);
+			return (
+				cacheRes ||
+				fetch(evt.request).then((fetchRes) => {
+					return caches.open(dynamicCacheName).then((cache) => {
+						cache.put(evt.request.url, fetchRes.clone());
+						return fetchRes;
+					});
+				})
+			);
 		})
 	);
 });
