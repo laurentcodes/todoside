f8ebbf0a836af56399fa00574f4f922f3f4710ff
laurentcodes <o.sherkes@gmail.com>
2020-05-28 01:34:13 +0100

    Added Functionality

--- script.js
+++ script.js
@@ -2,18 +2,86 @@ const addTodo = document.getElementById('add-todo');
 const todoInput = document.getElementById('new-todo-input');
 const footer = document.getElementById('footer');
 const submitTodo = document.getElementById('submit-todo');
+const todos = document.getElementById('todos');
+
+let todosData = JSON.parse(localStorage.getItem('todoSideTodos'));
+
+function checkLocalStorage() {
+	if ('todoSideTodos' in localStorage) {
+		updateDOM(todosData);
+	} else {
+		dummyData = [
+			'Welcome to TodoSide. 📝',
+			'Click New Todo to add a new Todo. ➕',
+			'Hover on a todo to show the delete button. 🚮',
+			'Click on a button to mark it as completed. ✅',
+		];
+		updateDOM(dummyData);
+
+		console.log('No Local Storage Item Available');
+	}
+}
+
+checkLocalStorage();
+
+function updateDOM(data) {
+	todos.innerHTML = '';
+	data.forEach((el) => {
+		const todo = `
+      <li>
+        ${el}
+        <i class="fas fa-trash-alt" id="delete"></i>
+      </li>
+    `;
+
+		addtoDOM(todo);
+	});
+}
 
 addTodo.addEventListener('click', () => {
-	footer.style.display = 'none';
-	todoInput.style.visibility = 'visible';
-	todoInput.style.opacity = 1;
+	todoInput.classList.add('show');
 });
 
 submitTodo.addEventListener('click', (e) => {
 	const parent = e.target.parentElement;
-	const todoItem = parent.children[0].value;
+	let todoItem = parent.children[0].value;
 
-	todoInput.style.visibility = 'hidden';
-	todoInput.style.opacity = 0;
-	footer.style.display = 'flex';
+	const todo = `
+  <li>
+    ${todoItem}
+    <i class="fas fa-trash-alt" id="delete"></i>
+  </li>
+  `;
+
+	todosData ? todosData : (todosData = []);
+
+	todosData.push(todoItem);
+
+	localStorage.setItem('todoSideTodos', JSON.stringify(todosData));
+	addtoDOM(todo);
+
+	todoInput.style.transition = 'all 0.3s ease-in-out;';
+	todoInput.classList.remove('show');
+
+	parent.children[0].value = '';
+
+	setTimeout(checkLocalStorage, 1000);
+});
+
+function addtoDOM(el) {
+	todos.insertAdjacentHTML('beforeend', el);
+}
+
+todos.addEventListener('click', (e) => {
+	if (e.target && e.target.matches('li')) {
+		e.target.classList.toggle('completed');
+	}
+
+	if (e.target && e.target.matches('i')) {
+		const el = e.target.parentElement;
+		el.style.opacity = '0';
+		setTimeout(function () {
+			el.remove();
+		}, 300);
+	}
 });
